f0e50096d98b086336de167780b73b62ed14eef8
robert lowry <lowryrob2001@gmail.com>
2019-07-09 09:46:24 -0700

    Added home controller

---
+++ AlmostEmptyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AlmostEmptyWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*An ASP.NET Action is a public method
+         * in a controller class
+         */
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
